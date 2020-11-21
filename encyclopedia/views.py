4925d3a642f77a3af9a71d4e392286e83eac573a
sflems <44352296+sflems@users.noreply.github.com>
2020-11-21 04:35:14 -0800

    Implemented Random Page
    Random Page link now works.
    
    How can "random": random.choice(entries) be passed to all functions instead of reapeated for all pages?

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -8,15 +8,18 @@ entries = util.list_entries()
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": entries,
+        "random": random.choice(entries)
     })
     
 def add(request):
     return render(request, "encyclopedia/add.html", {
-        "entries": entries
+        "entries": entries,
+        "random": random.choice(entries)
     })
     
 def entry(request, title):
     entry = util.get_entry(title)
     return render(request, "encyclopedia/entry.html", {
-        "entry": markdown2.markdown(entry)
+        "entry": markdown2.markdown(entry),
+        "random": random.choice(entries)
        })
