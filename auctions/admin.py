4d337b1c5f374e29547ea19c65dd0a24a8d613cd
sflems <44352296+sflems@users.noreply.github.com>
2020-11-25 01:03:53 -0800

    Listings Expanded

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import User
+from .models import User, Listing, Bid, Comment, Watchlist
 
 # Register your models here.
 admin.site.register(User)
+admin.site.register(Listing)
+admin.site.register(Bid)
+admin.site.register(Comment)
+admin.site.register(Watchlist)
--- auctions/models.py
+++ auctions/models.py
@@ -6,25 +6,25 @@ class User(AbstractUser):
     
 class Listing(models.Model):
     title = models.CharField(max_length=128, blank=False, unique=True)
-    description = models.CharField(max_length=1000, blank=False)
-    image_URL = models.URLField
-    start_price = models.PositiveIntegerField
-    start_date = models.DateField(auto_now_add=True)
-    end_date = models.DurationField(blank=True)
+    description = models.TextField(max_length=1000, blank=False)
+    image_URL = models.URLField(blank=True)
+    start_price = models.DecimalField(max_digits=12, decimal_places=2, default="00.00")
+    start_date = models.DateTimeField(auto_now_add=True)
+    end_date = models.DateField(null=True, blank=False)
     last_updated = models.DateField(auto_now=True)
-    user = User.get(name)
+    seller = models.ForeignKey(User, on_delete=models.CASCADE, related_name='seller')
     
-class Bids(models.Model):
+class Bid(models.Model):
     pass
 
-class Comments(models.Model):
-    listing = models.ForeignKey(Listing, related_name='comments')
-    author = models.ForeignKey(User, related_name='author')
+class Comment(models.Model):
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author')
     comment = models.CharField(max_length=500, blank=False)
     
 class Watchlist(models.Model):
     items = models.ManyToManyField(Listing, blank=True, related_name="watchlist")
-    watcher = models.ForeignKey(User, related_name='Watcher')
+    watcher = models.ForeignKey(User, on_delete=models.CASCADE, related_name='Watcher')
 
     def __str__(self):
         return f"{self.items}"
