3f3d5985fe9240751631ca7edcc3198bffecc343
sflems <44352296+sflems@users.noreply.github.com>
2020-11-25 00:17:43 -0800

    Comments Model, modified Watchlist Model

--- auctions/models.py
+++ auctions/models.py
@@ -12,15 +12,19 @@ class Listing(models.Model):
     start_date = models.DateField(auto_now_add=True)
     end_date = models.DurationField(blank=True)
     last_updated = models.DateField(auto_now=True)
+    user = User.get(name)
     
 class Bids(models.Model):
     pass
 
 class Comments(models.Model):
-    pass
+    listing = models.ForeignKey(Listing, related_name='comments')
+    author = models.ForeignKey(User, related_name='author')
+    comment = models.CharField(max_length=500, blank=False)
     
 class Watchlist(models.Model):
     items = models.ManyToManyField(Listing, blank=True, related_name="watchlist")
+    watcher = models.ForeignKey(User, related_name='Watcher')
 
     def __str__(self):
         return f"{self.items}"
