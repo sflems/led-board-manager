1cc4a2ebd7e9ac03ed4b1884f08da239c586d045
sflems <44352296+sflems@users.noreply.github.com>
2020-11-26 00:28:07 -0800

    Added Bid Model and Sample Entries

--- auctions/models.py
+++ auctions/models.py
@@ -8,23 +8,35 @@ class Listing(models.Model):
     title = models.CharField(max_length=128, blank=False, unique=True)
     description = models.TextField(max_length=1000, blank=False)
     image_URL = models.URLField(blank=True)
-    start_price = models.DecimalField(max_digits=12, decimal_places=2, default="00.00")
+    start_bid = models.DecimalField(max_digits=12, decimal_places=2, default="00.00")    
     start_date = models.DateTimeField(auto_now_add=True)
     end_date = models.DateField(null=True, blank=False)
     last_updated = models.DateField(auto_now=True)
-    seller = models.ForeignKey(User, on_delete=models.CASCADE, related_name='seller')
+    user = models.ForeignKey(User, blank=False, on_delete=models.CASCADE, related_name='sellers')
+    
+    def __str__(self):
+        return f"{self.title} - ${self.start_bid}"
     
 class Bid(models.Model):
-    pass
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name='listings', null=True)
+    bid = models.DecimalField(max_digits=12, decimal_places=2, default=0)
+    user = models.ForeignKey(User, blank=False, on_delete=models.CASCADE, related_name='bidder')
+    datetime = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"{self.listing.id}: {self.listing} - {self.user} - Bid: {self.bid}"
 
 class Comment(models.Model):
-    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name='comments')
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author')
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name='comments', null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='authors')
     comment = models.CharField(max_length=500, blank=False)
+    
+    def __str__(self):
+        return f"Listing: {self.listing} - {self.comment} - By: {self.user}"
     
 class Watchlist(models.Model):
     items = models.ManyToManyField(Listing, blank=True, related_name="watchlist")
-    watcher = models.ForeignKey(User, on_delete=models.CASCADE, related_name='Watcher')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='watchers')
 
     def __str__(self):
-        return f"{self.items}"
+        return f"{self.user}"
--- auctions/views.py
+++ auctions/views.py
@@ -4,11 +4,13 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-from .models import User
+from .models import User, Listing, Bid, Comment, Watchlist
 
 
 def index(request):
-    return render(request, "auctions/index.html")
+    return render(request, "auctions/index.html", {
+        "listings": Listing.objects.all()
+    })
 
 
 def login_view(request):
