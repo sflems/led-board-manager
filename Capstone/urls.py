81b0b13107b4ddd09cd8301f4170e52b34b2a353
Scott <flems17@gmail.com>
2021-01-23 18:41:02 -0800

    Django Base Project Files

--- Capstone/urls.py
+++ Capstone/urls.py
@@ -1,7 +1,7 @@
-"""project4 URL Configuration
+"""Capstone URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,9 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include("network.urls")),
+    path('admin/', admin.site.urls),
 ]
