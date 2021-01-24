324c48e5e01c9857bbcdc7cd1f1bf0f1026b41ec
Scott <flems17@gmail.com>
2021-01-23 18:58:58 -0800

    Installed Django Crispy Forms and Bootstrap4

--- Capstone/settings.py
+++ Capstone/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django-bootstrap4',
+    'django-crispy-forms',
 ]
 
 MIDDLEWARE = [
