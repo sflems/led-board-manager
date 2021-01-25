2b42fc25647bef870da0a665113f573ea9ee419e
sflems <44352296+sflems@users.noreply.github.com>
2021-01-24 19:35:24 -0800

    Added Settings Model
    New Settings model uses JSONField to import scoreboard/fixtures/config.json.sample as a field.
    
    This is in its infancy as the config can now be expanded out into JSONforms or as further model attributes for customization.

--- scoreboard/models.py
+++ scoreboard/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User, AbstractUser
-from django.forms import ModelForm
+from django.forms import ModelForm, Form
+from django_jsonforms.forms import JSONSchemaField
 from django.utils import timezone
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.utils.translation import gettext_lazy as _
 from django.core.exceptions import ObjectDoesNotExist
+import json
 
 # Default User Class
 class User(AbstractUser):
@@ -29,7 +31,6 @@ def save_user_profile(sender, instance, **kwargs):
     except ObjectDoesNotExist:
         Profile.objects.create(user=instance)
 
-# TO DO: Implement models for Teams, Stats, etc. 
 # Fixtures - import from JSON with manage.py loaddata
 class Team(models.Model):
     name = models.CharField(max_length=32, unique=True)
@@ -48,3 +49,22 @@ class Team(models.Model):
 
     def get_absolute_url(self):
         return reverse("Team_detail", kwargs={"pk": self.pk})
+
+
+# TO DO: Implement settings form to change settings and update active profile.
+class Settings(models.Model):
+    # Opens default config and loads into Settings Profile
+    def conf_default():
+        with open("./scoreboard/fixtures/config.json.sample", "r") as f:
+            return json.load(f)
+            
+    name = models.CharField(_("Config Name"), default="Default", max_length=32, blank=True)
+    config = models.JSONField(default=conf_default)
+    isActive = models.BooleanField(_("Active"),default=1)
+  
+    class Meta:
+        verbose_name = _("Settings")
+        verbose_name_plural = _("Settings")
+        
+    def __str__(self):
+        return self.name + " Profile"
