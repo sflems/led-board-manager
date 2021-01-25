90de8204697996ec24b0671163ca4628bfae7966
sflems <44352296+sflems@users.noreply.github.com>
2021-01-24 17:00:31 -0800

    List Display for Teams Admin Panel
    Ordered by teamName

--- scoreboard/admin.py
+++ scoreboard/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import Team, User
 
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ('id', 'teamName', 'abbreviation',)
+    ordering = ('teamName',)
+
 # Register your models here.
 admin.site.register(User)
-admin.site.register(Team)
+admin.site.register(Team, TeamAdmin)
