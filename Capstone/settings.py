8257c41196955ddcb03b925b125e2120f7aed7c7
sflems <44352296+sflems@users.noreply.github.com>
2021-01-26 00:17:32 -0800

    Decoupled

--- Capstone/settings.py
+++ Capstone/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -18,7 +19,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 't=5f#&*h^d9ynlpr!bllv!+i0flo6szwp^b@v+bbz5!4_ehhoa'
+# DECOUPLE! https://stackoverflow.com/questions/64208678/hiding-secret-key-in-django-project-on-github-after-uploading-project
+SECRET_KEY = config("SECRET_KEY") # this the secret key in .gitignore'd .env
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
