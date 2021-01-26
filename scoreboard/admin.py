5cc9d91e895d5701ac04217c565366416eb77899
sflems <44352296+sflems@users.noreply.github.com>
2021-01-25 21:10:27 -0800

    Added Settings to Admin

--- scoreboard/admin.py
+++ scoreboard/admin.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
 from .models import Settings, Team, User
 
+class SettingsAdmin(admin.ModelAdmin):
+    pass
+
 class TeamAdmin(admin.ModelAdmin):
     list_display = ('id', 'teamName', 'abbreviation',)
     ordering = ('teamName',)
 
 # Register your models here.
 admin.site.register(User)
-admin.site.register(Settings)
+admin.site.register(Settings, SettingsAdmin)
 admin.site.register(Team, TeamAdmin)
