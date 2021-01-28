09ce76532e2d743bc22f7362b3e4da68ba832bd8
sflems <44352296+sflems@users.noreply.github.com>
2021-01-27 22:30:45 -0800

    Removed Registration Pages
    Front end registration removed as a security measure. Users may not have their Pi or Networks configured correctly and the admin should be the only method to create a new user. It should be OK as the only users should be household members.

--- scoreboard/views.py
+++ scoreboard/views.py
@@ -29,7 +29,7 @@ def settings_view(request):
         #TO DO: Expand on form validation
         try:
             ''' 
-                The request data must be encoded and the decoded again as below due to a BOM error. Without this the form submissions are saved as slash escaped strings... but why? Possibly due to jsonforms JSON serialization method. (Needs expert opinon.)
+                The request data must be encoded and the decoded again as below due to a BOM error. Without this the form submissions are saved as slash escaped strings... but why? Possibly due to jsonforms JSON serialization method.
             '''
             new_config = request.POST['json'].encode().decode('utf-8-sig')
             new_settings = Settings.objects.create(config=json.loads(new_config))
@@ -67,30 +67,3 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     return HttpResponseRedirect(reverse("index"))
-
-
-def register(request):
-    if request.method == "POST":
-        username = request.POST["username"]
-        email = request.POST["email"]
-
-        # Ensure password matches confirmation
-        password = request.POST["password"]
-        confirmation = request.POST["confirmation"]
-        if password != confirmation:
-            return render(request, "scoreboard/register.html", {
-                "message": "Passwords must match."
-            })
-
-        # Attempt to create new user
-        try:
-            user = User.objects.create_user(username, email, password)
-            user.save()
-        except IntegrityError:
-            return render(request, "scoreboard/register.html", {
-                "message": "Username already taken."
-            })
-        login(request, user)
-        return HttpResponseRedirect(reverse("index"))
-    else:
-        return render(request, "scoreboard/register.html")
