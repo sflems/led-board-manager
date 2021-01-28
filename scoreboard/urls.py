f929985cfcd98ba6a8529fc7e53f948ef1a97132
sflems <44352296+sflems@users.noreply.github.com>
2021-01-27 23:34:11 -0800

    Removed Registration Page
    Front end registration removed as a security measure. Users may not have their Pi or Networks configured correctly and the admin should be the only method to create a new user. It should be OK as the only users should be household members.

--- scoreboard/urls.py
+++ scoreboard/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
     path("settings", views.settings_view, name="settings_view"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
-    path("register", views.register, name="register"),
 ]
