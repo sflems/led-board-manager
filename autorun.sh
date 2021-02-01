89678085d45fd7ce252c7370ede925276df1a99b
sflems <44352296+sflems@users.noreply.github.com>
2021-02-01 02:22:27 -0800

    Django Autorun Script
    /etc/rc.local contains the startup script command.
    chmod -x did not work, required chmod 0755 to run.

--- autorun.sh
+++ autorun.sh
@@ -2,6 +2,7 @@
 
 cd /home/flem/FlemSync
 source venv/bin/activate
-python /home/flem/FlemSync/Capstone/manage.py runserver 0:8085 --noreload &
+cd Capstone/
+python manage.py runserver 0:8085 --noreload &
 
 exit 0
