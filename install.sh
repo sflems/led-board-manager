d9ecb295e9bd9bf1ae6d61c59c3e1c9482e2620d
sflems <44352296+sflems@users.noreply.github.com>
2021-02-04 22:40:50 -0800

    webgui-log.out name change

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 exec 3>&1 4>&2
 trap 'exec 2>&4 1>&3' 0 1 2 3
-exec 1>log.out 2>&1
-# Everything below will go to the file 'log.out':
+exec 1>webgui-log.out 2>&1
+# Everything below will go to the file 'webgui-log.out':
 
 # Gets the current working dir. 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
