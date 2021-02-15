18ad9cd8c74c6d75fbe6422b9b13ce9011f3a1c0
Scott <flems17@gmail.com>
2021-02-15 00:47:23 -0800

    Removed demo mode check.

--- scoreboard/services.py
+++ scoreboard/services.py
@@ -21,10 +21,7 @@ def todays_games():
 
 # defines the users home/user/nhl-led-scoreboard/config folder path. Checks if DEMO_CS50 mode is enabled.
 def conf_path():
-    if settings.DEMO_CS50 != True:
-        path = os.path.dirname(settings.BASE_DIR) + "/nhl-led-scoreboard/config/"
-    else:
-        path = os.path.join(settings.BASE_DIR, 'demo/config/')
+    path = os.path.dirname(settings.BASE_DIR) + "/nhl-led-scoreboard/config/"
     return path
 
 # Opens default config from current config in the nhl-led-scoreboard folder if found, otherwise from static config, and then loads into Settings Profile
