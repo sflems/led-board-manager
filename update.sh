833a0852e43d07fba7818d54258556ba8504a0a6
sflems <44352296+sflems@users.noreply.github.com>
2021-03-04 18:50:43 -0800

    update.sh change to python3 path

--- update.sh
+++ update.sh
@@ -10,11 +10,10 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 echo "Working directory: "$DIR >&3
 
 cd $DIR
-source env/bin/activate
 
 # Install the app requirements and dependencies from the included requirements.txt file:
 echo "Installing requirements.txt. This may take a few moments..." >&3
-python3 -m pip install --ignore-installed -r requirements.txt >&3
+env/bin/python3 -m pip install --ignore-installed -r requirements.txt >&3
 
 # Create Django DB and load default data.
 echo "Updating WebGUI database and loading initial data..." >&3
@@ -24,7 +23,5 @@ python3 manage.py loaddata teams.json >&3
 
 echo "UPDATE COMPLETED!!!" >&3
 echo "Start the Web GUI server with 'gunicorn Capstone.wsgi -b 0:9002' or './autorun.sh'" >&3
-
-deactivate >&3
 
 exit 0
