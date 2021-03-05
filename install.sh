59a44aab6558ffb64c6cd607500abf2230d1a955
sflems <44352296+sflems@users.noreply.github.com>
2021-03-04 19:55:14 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -19,16 +19,12 @@ echo "Touching .secret.txt and updating permissions..." >&3
 # The .secret.txt file is automatically generated and saved here. It may also need to have the appropriate write permissions as well.
 touch .secret.txt >&3 && chmod g+w .secret.txt >&3 && echo "SUCCESS: Created and updated .secret.txt" >&3 || { echo "ERROR: Unable to create .secret.txt. Check webgui-log.out for details." >&3; exit 1; }
 
-# Start the venv
-source env/bin/activate >&3
-
 # Install the app requirements and dependencies from the included requirements.txt file:
 echo "Installing requirements.txt. This may take a few moments..." >&3
-python3 -m pip install --ignore-installed -r requirements.txt >&3
-
-echo "Generating WebGUI database and loading initial data..." >&3
+env/bin/python3 -m pip install --ignore-installed -r requirements.txt >&3
 
 # Create Django DB and load default data.
+echo "Generating WebGUI database and loading initial data..." >&3
 env/bin/python3 manage.py makemigrations >&3
 env/bin/python3 manage.py migrate >&3
 env/bin/python3 manage.py loaddata teams.json >&3
