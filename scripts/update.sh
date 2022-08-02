6d2fa5021d3b212c222577fa5002be536b39c8eb
sflems <scott@piboy.ca>
2022-08-02 00:30:22 -0700

    Update install/update scripts with basic fail messages.

--- scripts/update.sh
+++ scripts/update.sh
@@ -16,20 +16,20 @@ env/bin/python3 -m pip install -U -r requirements.txt >&3
 
 # Create Django DB, load default data, and run tests.
 echo "Updating WebGUI database and loading initial data..." >&3
-env/bin/python3 manage.py makemigrations >&3
-env/bin/python3 manage.py migrate >&3
-env/bin/python3 manage.py loaddata teams.json >&3
+env/bin/python3 manage.py makemigrations >&3 || echo "...FAILED. " >&3
+env/bin/python3 manage.py migrate >&3 || echo "...FAILED. " >&3
+env/bin/python3 manage.py loaddata teams.json >&3 ||
 
 echo "Running Django tests..." >&3
-env/bin/python3 manage.py test >&3 && echo "...done. " >&3
+env/bin/python3 manage.py test >&3 && echo "...done. " >&3 || echo "...FAILED. " >&3
 
 echo "Updating supervisor configurations..." >&3
-sudo supervisorctl reread >&3 && echo "...(1/3) - done. " >&3
-sudo supervisorctl reload >&3 && echo "...(2/3) - done. " >&3
+sudo supervisorctl reread >&3 && echo "...(1/3) - done. " >&3 || echo "...(1/3) - FAILED. " >&3
+sudo supervisorctl reload >&3 && echo "...(2/3) - done. " >&3 || echo "...(2/3) - FAILED. " >&3
 echo "Updating program configurations" >&3
-sleep 3 && sudo supervisorctl update >&3 && echo "...(3/3) - done. " >&3
+sleep 3 && sudo supervisorctl update >&3 && echo "...(3/3) - done. " >&3 || echo "...(3/3) - FAILED. " >&3
 
-echo "$(tput bold)UPDATE COMPLETED!!!" >&3
+echo "$(tput bold)No failures? UPDATE COMPLETED!!!" >&3
 echo "$(tput sgr0)Start the Web GUI server with $(tput bold)'source env/bin/activate && gunicorn Capstone.wsgi -b 0:9002'$(tput sgr0) or $(tput bold)'./scripts/autorun.sh'$(tput sgr0)" >&3
 
 exit 0
